9fa477388e161955629d30777077894147e001eb
Jonathan E. Emmett <jon@emmettmedia.com>
2023-05-08 17:02:47 -0500

    I have no idea take 2

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const app = require('express')();
+// index.js
+const express = require('express');
 
-app.get('/api', (req, res) => {
-	res.end({ message: 'Hello from server!' });
+const app = express();
+const PORT = 4000;
+
+app.listen(PORT, () => {
+	console.log(`API listening on PORT ${PORT} `);
 });
 
+app.get('/', (req, res) => {
+	res.send('Hey this is my API running 🥳');
+});
+
+app.get('/about', (req, res) => {
+	res.send('This is my about route..... ');
+});
+
+// Export the Express API
 module.exports = app;
