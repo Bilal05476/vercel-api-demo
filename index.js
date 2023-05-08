631761e79dcd8d13b3f254078db5a8906b11e154
Jonathan E. Emmett <jon@emmettmedia.com>
2023-05-08 16:45:28 -0500

    json file tweak

--- index.js
+++ index.js
@@ -9,16 +9,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/api', (req, res) => {
-	const path = `/api/item/${v4()}`;
+app.get('/', (req, res) => {
 	res.setHeader('Content-Type', 'text/html');
 	res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-	res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-	const { slug } = req.params;
-	res.end(`Item: ${slug}`);
+	res.send('Vercel API...');
 });
 
 app.listen(PORT, () => {
