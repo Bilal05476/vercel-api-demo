d452adad924dbcbf10e09fe4a335e57b76ab2ffa
Jonathan E. Emmett <jon@emmettmedia.com>
2023-05-08 16:34:53 -0500

    this will work

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const app = require('express')();
-const { v4 } = require('uuid');
+import express from 'express';
+import cors from 'cors';
+const PORT = process.env.PORT || 5100;
 
-app.get('/api', (req, res) => {
-	const path = `/api/item/${v4()}`;
-	res.setHeader('Content-Type', 'text/html');
-	res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-	res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('Vercel API...');
 });
 
-app.get('/api/item/:slug', (req, res) => {
-	const { slug } = req.params;
-	res.end(`Item: ${slug}`);
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
 });
-
-module.exports = app;
