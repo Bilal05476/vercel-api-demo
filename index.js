7997f6e5f717fa76e3c75b6c7757ddeb6c58d1b6
Jonathan E. Emmett <jon@emmettmedia.com>
2023-05-08 18:12:00 -0500

    removed cors

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require('express')();
-const cors = require('cors');
 
 const app = express;
-
-app.use(cors());
-// app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.status(200).json({ message: 'Hello World' });
