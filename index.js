670be64fdf9a9fc224076015e33a327fbd00f4ec
Jonathan E. Emmett <jon@emmettmedia.com>
2023-05-08 18:10:15 -0500

    added cors

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const cors = require('cors');
 const app = express;
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-	res.end('Hello World');
+	res.status(200).json({ message: 'Hello World' });
+	// res.end('Hello World');
 });
 
 app.get('/hello', (req, res) => {
