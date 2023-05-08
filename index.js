54a35235768e4226cd761347e69606a408d8a819
Jonathan E. Emmett <jon@emmettmedia.com>
2023-05-08 16:27:57 -0500

    demo from vercel

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-const PORT = process.env.PORT || 5100;
+const app = require('express')();
+const { v4 } = require('uuid');
 
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
-app.get('/', (req, res) => {
-	res.send('Vercel API...');
+app.get('/api', (req, res) => {
+	const path = `/api/item/${v4()}`;
+	res.setHeader('Content-Type', 'text/html');
+	res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+	res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-export default app;
+app.get('/api/item/:slug', (req, res) => {
+	const { slug } = req.params;
+	res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
