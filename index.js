548d8f2a0d0ad3e182902592a00ddc88be7245e0
Jonathan E. Emmett <jon@emmettmedia.com>
2023-05-08 18:20:39 -0500

    added mongoose and config

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')();
 const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+
+require('dotenv').config();
+connectDB();
 
 const app = express;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
 	res.status(200).json({ message: 'Hello World' });
