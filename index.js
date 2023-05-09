431c46d7a6f5436f366e54e6fe3654b319c184ef
Jonathan E. Emmett <jon@emmettmedia.com>
2023-05-09 08:07:27 -0500

    ugh take 3

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import userRoutes from './routes/userRoutes.js';
 dotenv.config();
 connectDB();
 
-const app = express;
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
